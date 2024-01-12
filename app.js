ea77930e9c552de2a0ff75b27b54391c3c52c1c3
Autumn Brown <autumn.brown22@outlook.com>
2024-01-12 08:40:59 -0500

    new commit adding capstone

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+// const express = require('express')
+// // const mongoose = require('mongoose')
+// const queries = require('./queries.js')
+
+
+
+// // const bodyParser = require('body-parser')
+// const app = express()
+// const tokenManager = require('./token-manager.js')
+
+// const cors = require('cors')
+
+
+//SQL setup/ express config
+app.use(cors())
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+)
+
+
+app.listen(3000)
+console.log("Express is running")
---
+++ queries.js
@@ -0,0 +1,4 @@
+// const { model } = require("mongoose")
+// const Model = require("./schema.js")
+// const Pool = require('pg').Pool
+// require('dotenv').config()
